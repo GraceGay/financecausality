dc1c97282d484f759260d7378a3484316a3bfb47
GraceGay <646199191@qq.com>
2017-04-24 16:26:46 +0800

    read text file line by line

--- src/main/java/edu/sjtu/adapt/ChineseSegment.java
+++ src/main/java/edu/sjtu/adapt/ChineseSegment.java
@@ -1,11 +1,15 @@
 package edu.sjtu.adapt;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+
+import org.hibernate.validator.constraints.SafeHtml.WhiteListType;
 
 import edu.stanford.nlp.io.IOUtils;
 import edu.stanford.nlp.ling.CoreAnnotations;
@@ -26,17 +30,21 @@ public class ChineseSegment {
 		System.out.println("filein:"+filein);
 		System.out.println("fileout:"+fileout);
 		StanfordCoreNLP pipeline = new StanfordCoreNLP("StanfordCoreNLP-chinese-tokenize.properties");
-		 String text = IOUtils.stringFromFile(filein,StandardCharsets.UTF_8.name());
+		BufferedReader bReader=Files.newBufferedReader(Paths.get(fileout));
+		BufferedWriter bWriter = Files.newBufferedWriter(Paths.get(fileout));
+		while(bReader.ready()){
+		String text=bReader.readLine();
 		Annotation annotation = new Annotation(text);
 		pipeline.annotate(annotation);
 		List<CoreMap> sens = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-		BufferedWriter bWriter = Files.newBufferedWriter(Paths.get(fileout));
 		for (CoreMap sen : sens) {
 			String tokens = sen.get(CoreAnnotations.TokensAnnotation.class).stream()
 					.map(x -> x.get(CoreAnnotations.TextAnnotation.class)).reduce((x, y) -> x + " " + y).orElse("");
 			System.out.println(tokens);
 			bWriter.write(tokens);
+			}
 		}
+		bReader.close();
 		bWriter.close();
 	}
 }
