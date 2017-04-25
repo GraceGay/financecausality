865289f62c8afbfbbc1595e3ec734d7f93d8d2ed
GraceGay <646199191@qq.com>
2017-04-25 13:13:01 +0800

    write the process.java

---
+++ src/main/java/edu/sjtu/adapt/Process.java
@@ -0,0 +1,46 @@
+package edu.sjtu.adapt;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Process {
+
+	public static void main(String[] args) throws IOException {
+		HashSet<String>causekeys=new HashSet<>();
+		HashSet<String>effectkeys=new HashSet<>();
+		effectkeys.addAll(Arrays.asList("原来" ,"以便" ,"是故" ,"以致" ,"因为" ,"因此" ,"所以" ,"由于" ,"致使" ,"因而" ,"只怪" ,"这说明" ,"表明了","这表明" ,"出于" ,"起源于" ,"来自于" ,"的原因" ,"原因是" ,"取决于" ,"导致" ));
+		causekeys.addAll(Arrays.asList("原来"  ,"是故"  ,"因为"  ,"由于"   ,"只怪" ,"出于" ,"起源于" ,"来自于" ,"的原因" ,"原因是" ,"取决于" ));
+		effectkeys.removeAll(causekeys);
+//		System.out.println(causekeys.size());
+//		System.out.println(effectkeys.size());
+//		System.out.println(causekeys.stream().reduce((x,y)->x+"*"+y));
+//		System.out.println(effectkeys.stream().reduce((x,y)->x+"*"+y));
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		
+		BufferedReader reader = Files.newBufferedReader(Paths.get(args[0]));
+		BufferedWriter writer = Files.newBufferedWriter(Paths.get(args[1]));
+		reader.lines().parallel().filter(line->{
+		return causekeys.stream().anyMatch(key->line.indexOf(key)>0)&
+						   effectkeys.stream().anyMatch(key->line.indexOf(key)>0);	
+		}).forEach(line->{
+			try {
+				writer.write(line+"\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});	
+		reader.close();
+		writer.close();
+		
+		
+		
+		
+	}
+
+}
--- src/main/java/edu/sjtu/adapt/SelectSentence.java
+++ src/main/java/edu/sjtu/adapt/SelectSentence.java
@@ -7,7 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.stream.Stream;
 
 public class SelectSentence {
 
@@ -21,12 +20,11 @@ public class SelectSentence {
 		}
 		HashSet<String>keys=new HashSet<>();
 		keys.addAll(Arrays.asList("原来" ,"以便" ,"是故" ,"以致" ,"因为" ,"因此" ,"所以" ,"由于" ,"致使" ,"因而" ,"只怪" ,"这说明" ,"表明" ,"出于" ,"起源于" ,"来自于" ,"的原因" ,"原因是" ,"取决于" ,"导致" ));
-		Stream<String> keysStream=keys.stream();
 		BufferedReader reader = Files.newBufferedReader(Paths.get(filein));
 		BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileout));
 		reader.lines().parallel()
-		.flatMap(x->Arrays.asList(x.split("。|！|!|？|?")).stream())
-		.filter(x->keysStream.anyMatch(key->x.indexOf(key)>0))
+		.flatMap(x->Arrays.asList(x.split("。|！|!|？|\\?")).stream())
+		.filter(x->keys.stream().anyMatch(key->x.indexOf(key)>0))
 		.forEach(x->{
 			try {
 				writer.write(x+"\n");
